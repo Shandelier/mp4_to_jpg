4daf645b7597250945cc022a88d7afc472f8a65c
Carlos <26363920+Shandelier@users.noreply.github.com>
2021-02-16 17:17:16 +0100

    better i/o

--- vid2pic.py
+++ vid2pic.py
@@ -16,26 +16,54 @@ import numpy as np
 import utils as ut
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input', type=str,
-                    default=r"C:\Users\kluse\Documents\python\SSR-Dataset\test")
+parser.add_argument('--vid_dir', type=str,
+                    default="./videos")
 parser.add_argument('--output', type=str,
-                    default=r"C:\Users\kluse\Documents\python\SSR-Dataset")
+                    default="./output")
 parser.add_argument('--downsampling', type=int,
-                    default=1)
+                    default=10)
 args = parser.parse_args()
 
 
-def split_frame_file(file, out_path, downsampling=10):
+def main():
+    check_same_path()
+    files = [
+        f.path
+        for f in os.scandir(args.vid_dir)
+        if f.is_file() and f.path.endswith((".mp4"))
+    ]
+    print("files found: ", files)
+    for file in files:
+        vid_name = os.path.basename(file).split('.', 1)[0]
+        output_dir = args.output + "/" + vid_name
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        split_frame_file(file, output_dir)
+    print("[  INFO  ] Extraction finished")
+
+
+def check_same_path():
+    if args.vid_dir == args.output:
+        print(
+            "[WARNING] input dir is the same as output dir -- the pictures will be overwritten"
+        )
+        print("Do you wish to continue?: y/n")
+        if input() != "y":
+            exit()
+
+
+def split_frame_file(file, out_dir):
     cap = cv2.VideoCapture(file)
-    out = out_path
+    vid_name = os.path.basename(file).split('.', 1)[0]
 
     # posenet train picture size: 353x257
     # but video size is in 16:9 aspect ratio
-    dsize = (457, 257)
-    # TODO: cropping
+    dsize = (320, 180)
+    # TODO: cropping to native posenet size
     # cropsize = (353, 257)
     # crop_img = img[y:y+h, x:x+w]
 
+    path = ""
     n_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     print("{} vid of {} estimated frames".format(
         os.path.basename(file), n_frames))
@@ -45,31 +73,18 @@ def split_frame_file(file, out_path, downsampling=10):
         ret, frame = cap.read()
         if ret:
             frame = cv2.resize(frame, dsize)
-            path = "{}/{}_{}.jpg".format(out,
-                                         os.path.splitext(file)[0], str(i))
+            path = "{}/{}.jpg".format(out_dir, str(i))
             cv2.imwrite(path, frame)
-            i += downsampling
-            if not i % (n_frames/10):
+            i += args.downsampling
+            if not (i % (n_frames/10)):
                 print("{}/% of video ready".format(i/n_frames*100))
             cap.set(1, i)
         else:
             break
     cap.release()
-    n_samples = len(os.listdir(out))
-    print("{} samples extracted".format(n_samples))
+    n_samples = len(os.listdir(out_dir))
+    print("[FINISH] {} samples extracted".format(n_samples))
 
 
-downsampling = args.downsampling
-
-src_path = args.input + r"/"
-files = ut.dir2files(src_path)
-print("files found: ", files)
-
-out_path = args.output + r"/pictures"
-if not os.path.isdir(out_path):
-    os.makedirs(out_path)
-
-for file in files:
-    split_frame_file(file, out_path, downsampling)
-
-print("[  INFO  ] Extraction finished")
+if __name__ == "__main__":
+    main()
